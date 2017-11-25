bd16cbe01b3b3c412cc242eb3d9d63c527581c3d
Kriszti Tóth <toth.kriszti@yahoo.com>
2017-11-25 19:22:22 +0100

    feat: summary with yelp not working yet

--- controllers/summarize-data.js
+++ controllers/summarize-data.js
@@ -1,43 +1,10 @@
-// helper functions:
-const constructUrl = (items) => {
-  const arr = [];
-  items.forEach((el) => {
-    arr.push(`${el.prefix}${el.width}x${el.height}${el.suffix}`);
-  });
-  return arr;
-};
-
-const getBestPhoto = (el) => {
-  const url = (`${el.prefix}${el.width}x${el.height}${el.suffix}`);
-  return url;
-};
-
-const extractCategories = (items) => {
-  const arr = [];
-
-  items.forEach((el) => {
-    arr.push(el.name, el.pluralName, el.shortName);
-  });
-  return arr;
-};
-
-const extractCategoriesYelp = (items) => {
-  const arr = [];
-
-  items.forEach((el) => {
-    arr.push(el.alias, el.title);
-  });
-  return arr;
-};
-
-const getYelpPrice = (items) => {
-  return items.length
-}
+  // create object structure
+  const summaryData = { ratings: {}, location: {}, photos: [], price: {}, categories: [] };
 
 // this function summarizes the data for the front-end
 const mingleData = async (googleData, foursquareData, yelpData) => {
-  // create object structure
-  const summaryData = { ratings: {}, location: {}, photos: [{}] };
+
+
   // add basic data
   summaryData.name = googleData.result.name;
   summaryData.place_id = googleData.result.place_id;
@@ -45,25 +12,66 @@ const mingleData = async (googleData, foursquareData, yelpData) => {
   summaryData.phone = googleData.result.international_phone_number;
   summaryData.location = googleData.result.geometry.location;
   // add ratings
-  summaryData.rating = (((googleData.result.rating) * 2) / 1).toFixed(1);
+  summaryData.rating = googleData.result.rating * 2;
   // add photos
   summaryData.ratings.google = googleData.result.rating;
-  summaryData.ratings.yelp = yelpData.rating;
+
+
   // add foursquare data
-  if (foursquareData && yelpData) {
-    console.log('HERE', yelpData);
+  if (foursquareData) {
     summaryData.ratings.foursquare = foursquareData.rating;
-    summaryData.rating = (((googleData.result.rating * 2) + foursquareData.rating + (yelpData.rating * 2)) / 3).toFixed(1);
     summaryData.bestPhoto = getBestPhoto(foursquareData.bestPhoto);
-    summaryData.photos = constructUrl(foursquareData.photos.groups[0].items).concat(yelpData.photos);
-    summaryData.categories = extractCategories(foursquareData.categories).concat(extractCategoriesYelp(yelpData.categories));
+    // summaryData.price.foursquare = foursquareData.price.tier
+    extractCategoriesFoursquare(foursquareData.categories);
+    photosFoursquare(foursquareData.photos.groups[0].items);
   }
-  if (foursquareData.price || yelpData.price) {
-    summaryData.price_range = ((foursquareData.price.tier + getYelpPrice(yelpData.price)) / 2).toFixed(0);
+  // add Yelp data
+
+  if (yelpData) {
+    console.log('HERE', yelpData);
+    summaryData.ratings.yelp = yelpData.rating;
+    summaryData.photos = yelpData.photos;
+    extractCategoriesYelp(yelpData.categories);
+    getYelpPrice(yelpData.price);
   }
 
+  if (foursquareData.price && yelpData.price) {
+    summaryData.rating = (((googleData.result.rating * 2) + foursquareData.rating) / 2).toFixed(1)
+    summaryData.price_range = ((foursquareData.price.tier + getYelpPrice(yelpData.price)) / 2).toFixed(0);
+    summaryData.rating = (((googleData.result.rating * 2) + foursquareData.rating + (yelpData.rating * 2)) / 3).toFixed(1);
+    summaryData.rating = (((googleData.result.rating * 2) + foursquareData.rating) / 2).toFixed(1);
+
+  }
 
   return summaryData;
 };
+
+// helper functions:
+const photosFoursquare = (items) => {
+  items.forEach((el) => {
+    foursquareData.photos.groups[0].items.push(`${el.prefix}${el.width}x${el.height}${el.suffix}`);
+  });
+};
+
+const getBestPhoto = (el) => {
+  const url = (`${el.prefix}${el.width}x${el.height}${el.suffix}`);
+  return url;
+};
+
+const extractCategoriesFoursquare = (items) => {
+  items.forEach((el) => {
+    summaryData.categories.push(el.name, el.pluralName, el.shortName);
+  });
+};
+
+const extractCategoriesYelp = (items) => {
+  items.forEach((el) => {
+    summaryData.categories.push(el.alias, el.title);
+  });
+};
+
+const getYelpPrice = (items) => {
+  return items.length
+}
 
 module.exports = mingleData;
