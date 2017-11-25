ce96f5ee80000b76e187e815f6289c0b37f45691
Chad <chadhamre@gmail.com>
2017-11-25 12:32:53 +0100

    feat: initial foursquare data

--- controllers/summarize-data.js
+++ controllers/summarize-data.js
@@ -1,5 +1,6 @@
 // this function summarizes the data for the front-end
-const mingleData = async (googleData) => {
+const mingleData = async (googleData, foursquareData) => {
+
   // create object structure
   const summaryData = { ratings: {}, photos: {} };
   // add basic data
@@ -9,8 +10,14 @@ const mingleData = async (googleData) => {
   summaryData.phone = googleData.result.international_phone_number;
   // add ratings
   summaryData.rating = ((googleData.result.rating * 2) / 1).toFixed(1);
-  summaryData.ratings.google = googleData.result.rating;
   // add photos
+  summaryData.ratings.google = googleData.result.rating;
+  // add foursquare data
+  if (foursquareData) {
+    summaryData.ratings.foursquare = foursquareData.rating;
+    summaryData.rating = (((googleData.result.rating * 2) + foursquareData.rating ) / 2).toFixed(1);
+  }
+
 
   return summaryData;
 };
