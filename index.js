262ad9debffff7a23458fc43b421814f3169f3c7
Chad <chadhamre@gmail.com>
2017-12-02 18:13:27 +0100

    feat: add redis cache

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ require('dotenv').config();
 const Koa = require('koa');
 const logger = require('koa-logger');
 const router = require('./router/routes');
+const cache = require('koa-response-cache');
 
 const app = new Koa();
 const PORT = process.env.PORT || 4000;
 
+const REDIS_HOST = process.env.REDIS_URL.split(':')[0];
+const REDIS_PORT = process.env.REDIS_URL.split(':')[1];
+
+const options = {
+  expire: 60,
+  routes: ['/api/(.*)'],
+  redis: {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  },
+};
+
 app
   .use(logger())
+  .use(cache(options))
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(process.env.PORT || 4000);
+  .listen(PORT);
